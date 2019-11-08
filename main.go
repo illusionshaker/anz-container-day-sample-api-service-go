5dae955da32f7e7a95cee54047f139213e1d41ce
Mathew Ng <mathewng@hotmail.com>
2019-11-08 13:37:48 +1100

    Update the text message

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "I've made some changes"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
